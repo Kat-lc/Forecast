07aa5b966b8467d08d5175370edebc472d0a9e69
Kat-lc <cussansk@mail.gvsu.edu>
2020-06-13 22:18:33 -0400

    Changed the header design

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,11 @@ import ReactDOM from 'react-dom';
 export class Header extends React.Component {
     render() {
         return (
-            <nav class="navbar navbar-light bg-light">
-                <span class="navbar-brand mb-0 h1">Navbar</span>
+            <nav class="navbar navbar-custom">
+                <span class="navbar-brand mb-0 h1">Weather Report</span>
+                <span class="navbar-text white-text">
+                    By Katie Cussans and Gino LaGuardia-LoBianco
+                </span>
             </nav>
         );
     }
