91b04feebcb10500784d519168e3d434be073665
cafetechne <cafetechne@gmail.com>
2020-06-14 23:49:19 -0400

    Added my final contribution to the project:
    Added:
    
    -dynamic rendering of Table object in render() method of Location.js
    
    -styling of Table component in Location.css to address font and image formatting issues and responsiveness.
    
    -comments in Index.js and Location.js

--- src/Location.js
+++ src/Location.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Jumbotron from './Jumbotron';
+import Table from 'react-bootstrap/Table';
+import Image from 'react-bootstrap/Image';
 
+
+/**
+ * The Location component defined below is called in the render of App.js.
+ * The purpose of this component is to render a view displaying the
+ * weather information from wherever the user is viewing the web app from.
+ * It queries the user's geolocation data, from that data pulls a code, 
+ * queries the National Weather Service API for the forecast related to
+ * that grid code, and then renders it using react-bootstrap components.
+ */
 export class Location extends React.Component {
+    //The constructor initializes the data in this.state
     constructor(props) {
         super(props);
         this.state = {
@@ -18,10 +29,15 @@ export class Location extends React.Component {
 
     }
 
+    //The location is set here and calls set position to be the
+    //argument of the calling geolocation function
     componentDidMount = () => {
         navigator.geolocation.getCurrentPosition(this.setPosition);
     }
 
+    //The setPosition function stores the geolocation data
+    //as a data member of the calling object, which was possible
+    //due to the binding lambda syntax used on componentDidMount()
     setPosition = (location) => {
         this.setState({
             isLoaded: true,
@@ -30,6 +46,18 @@ export class Location extends React.Component {
         });
     }
 
+
+    /*
+     * The method of chaining fetch calls implemented in the below componenetDidUpdate()
+     * function where motivated by the selected answer to this link:
+     * https://stackoverflow.com/questions/40981040/using-a-fetch-inside-another-fetch-in-javascript  
+     * If the latitude and longitude coordinates need updating after comparison to the 
+     * previous state, two AJAX calls are preformed in a chain. PrevProps and PrevStates 
+     * are both required in order to prevent this from method from infinitely looping, which 
+     * happens when setState is called within this method without a conditional. 
+     * The first fetch grabs the forecast URL and sets the city and state values. 
+     * Then the. second fetch uses that forecast URL to grab the forecast "periods
+     */
     componentDidUpdate(prevProps, prevState) {
         if(this.state.lat !== prevState.lat && this.state.lon !== prevState.lon) {
             let url = `https://api.weather.gov/points/${this.state.lat},${this.state.lon}`
@@ -68,24 +96,39 @@ export class Location extends React.Component {
         } 
     }
 
+    /*
+     * The render method checks if an error occurred, if it did, then it returns
+     * the appropriate error message, otherwise, it will message the user that it
+     * loading until it returns a div containing a Bootstrap Jumbotron and a Table 
+     * with Image components embedded. It dynamically renders the table by mapping
+     * over each data entry and rendering the relevant particibles using JSX.
+    */
     render() {
         const { error, isLoaded, periods} = this.state;
 
-        if(error) {
+        if(error) { 
             return <div>Error: {error.message}</div>;
-        } else if(!isLoaded) {
+        } else if(!isLoaded) { 
             return <div>Loading...</div>;
         } else {
             return (
                 <div className="forecast">
                 <Jumbotron recentForecast={this.state.periods[0]} city={this.state.city} state={this.state.state}/>
-                <ul>
-                    {periods.map(item => (
-                        <li key={item.number}>
-                            {item.name} {item.temperature} {item.shortForecast}
-                        </li>
-                    ))}
-                </ul>
+        <Table  className="location" striped bordered hover responsive size="sm">
+        <tbody>
+          {
+            periods.map(item => (
+              <tr key={item.number}>
+                <td><Image  src={item.icon} fluid /></td>
+                <td><p>{item.name}</p></td>
+                <td><p>{item.shortForecast}</p></td>
+                <td><p>{item.temperature}</p></td>
+                <td><p>{item.windSpeed}</p></td>
+                <td><p>{item.windDirection}</p></td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
             </div>
             );
         }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+/**
+ * Katie Cussans & Gino LaGuardia-LoBianco
+ * CIS371: Web Application Development
+ * 
+ * Project #4: Weather Service Application using the National Weather Service API, Bootstrap and React
+ *
+ * This application queries the user's device for their location and then uses that data
+ * to send a request to the National Weather Service API and displays the forecast retrieved
+ * from the NWS database using React and Bootstrap.
+ * 
+ * ********************************************************************************************
+ * NOTE: The bulk of the documentation and implementation details are in the Location.js file 
+ * ********************************************************************************************
+ */
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './Header.css';
 import './Jumbotron.css';
+import './Location.css';
 import Header from './Header';
 import Location from './Location';
-import Jumbotron from './Jumbotron';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
